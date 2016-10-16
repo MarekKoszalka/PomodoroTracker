6250fd8448699bfe77a1fd6a75cb2a6c5a1ccb7b
Marek Koszałka <marek.koszalka94@gmail.com>
2016-10-16 14:19:00 +0200

    Learning how to commit properly

--- src/pomodorotracker/PomodoroMainWindow.java
+++ src/pomodorotracker/PomodoroMainWindow.java
@@ -3,13 +3,15 @@ package pomodorotracker;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Date;
+import java.util.*;
 import javax.swing.JLabel;
 
 /**
@@ -24,15 +26,14 @@ public class PomodoroMainWindow extends JFrame implements ActionListener{
     private final Container container;
     private final BorderLayout containerBorderLayout;
     //TODO zamienic te tablice buttonow na LISTę
-    private final JButton[] jButtonArray;
+    private final ArrayList<JButton> jButtonArrayList;
     private final JPanel jCenterPanel;
     private final JLabel jLabel;
     private final PomodoroTimer pomodoroTimer;
     private Timer timer;
-    int buttonsCount = 3;
 //++++++++++++++++++++++++CONSTRUCTOR+++++++++++++++++++++++++++++++++++++++++++
     public PomodoroMainWindow(){
-        pomodoroTimer = new PomodoroTimer(100000);
+        pomodoroTimer = new PomodoroTimer(10000);
         timer = new javax.swing.Timer(ONE_SEC, this);
         buttonMode = START_BUTTON_MODE;
 //------------------------CONTAINER AND LAYOUTS-???-----------------------------
@@ -40,62 +41,63 @@ public class PomodoroMainWindow extends JFrame implements ActionListener{
         jCenterPanel = new JPanel();
         containerBorderLayout = new BorderLayout();
         container.setLayout(containerBorderLayout);
+         
 //-------------------------BUTTONS----------------------------------------------        
-        jButtonArray = new JButton[buttonsCount];
-        jButtonArray[0] = new JButton("START");
-        jButtonArray[1] = new JButton("STOP");
-        jButtonArray[2] = new JButton("RESUME");
-        jButtonArray[0].setBackground(Color.GREEN);
-        jButtonArray[1].setBackground(Color.RED);
-        jButtonArray[2].setBackground(Color.ORANGE);
-        jButtonArray[0].addActionListener(this);
-        jButtonArray[1].addActionListener(this);
-        jButtonArray[2].addActionListener(this);
-        container.add(jCenterPanel, BorderLayout.CENTER);
-//------------------------------------------------------------------------------        
-        for(int i=0; i < buttonsCount; i++){
-            jCenterPanel.add(jButtonArray[i]);
+        jButtonArrayList = new ArrayList();
+        jButtonArrayList.add(new JButton("START"));
+        jButtonArrayList.add(new JButton("STOP"));
+        
+        jButtonArrayList.get(0).setBackground(Color.GREEN);
+        jButtonArrayList.get(1).setBackground(Color.RED);
+        
+        for(int i=0; i< jButtonArrayList.size(); i++){
+            jButtonArrayList.get(i).addActionListener(this);
+            jButtonArrayList.get(i).setPreferredSize(new Dimension(350, 130));
+            jButtonArrayList.get(i).setFont(new Font("Times New Roman", Font.BOLD, 25));
+            jCenterPanel.add(jButtonArrayList.get(i));
         }
         jLabel = new JLabel();
+        jLabel.setFont(new Font("Times New Roman", Font.BOLD, 80));
         jCenterPanel.add(jLabel);
+        container.add(jCenterPanel, BorderLayout.CENTER);
     }
 //++++++++++++++++++++METHODS OF POMODORO MAIN WINDOW+++++++++++++++++++++
     
 //ACTION LISTENER-----------------------------------------------
     @Override
-    //TODO dodać jakis Timer-odswiezajacy wartosc czasu...
     //TODO dodać formatowanie czasu z milisekund na jakiś przyjazny format !
     public void actionPerformed(ActionEvent e){
-        if      (e.getSource() == jButtonArray[0]){
-            
+        if      (e.getSource() == jButtonArrayList.get(0)){
             switch(buttonMode){
                 case 1:{
                     pomodoroTimer.start();
                     timer.start();
                     jLabel.setText(String.valueOf((pomodoroTimer.getActualTimeLeft()/1000)));
                     buttonMode = RESUME_BUTTON_MODE;
-                    jButtonArray[0].setBackground(Color.ORANGE);
-                    jButtonArray[0].setText("RESUME");
+                    jButtonArrayList.get(0).setBackground(Color.ORANGE);
+                    jButtonArrayList.get(0).setText("RESUME");
                     //TODO pomyslec nad napisaniem jakiejs funkcji obslugujacej to zmiane 
                 }
                 break;
                 case 2:{
+                    pomodoroTimer.resume();
                     if(pomodoroTimer.getIfTicking()){
                     jLabel.setText(String.valueOf(pomodoroTimer.getActualTimeLeft()/1000));
                     buttonMode = START_BUTTON_MODE;
-                    //TODO dodac zmiane na START button ponownie
+                    jButtonArrayList.get(0).setBackground(Color.GREEN);
+                    jButtonArrayList.get(0).setText("START");
                     }
                 }
             }
         }
                 
-        else if (e.getSource() == jButtonArray[1]){
+        else if (e.getSource() == jButtonArrayList.get(1)){
             pomodoroTimer.stop();
         }
-        else if (e.getSource() == jButtonArray[2]){
-            pomodoroTimer.resume();
-        }
         else if (e.getSource() == timer){
+            if(pomodoroTimer.getActualTimeLeft() <= 0){
+                pomodoroTimer.stop();
+            }
             if(pomodoroTimer.getIfTicking()){
                 jLabel.setText(String.valueOf(pomodoroTimer.getActualTimeLeft()/1000));
             }
