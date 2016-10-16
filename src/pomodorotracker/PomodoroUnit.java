ef9f4ad339cc01e5de3b46b906214db4922a536d
Marek Koszałka <marek.koszalka94@gmail.com>
2016-10-16 13:48:06 +0200

    new class Pomodoro Unit

---
+++ src/pomodorotracker/PomodoroUnit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pomodorotracker;
+
+/**
+ *
+ * @author Marek
+ */
+public class PomodoroUnit {
+//+++++++++++++++++++++++++++++++++++++++++++++++++++VARIABLES AND OBJECTS++++++
+    private String category;
+    private String description;
+    private int    duration;//TODO duration should have some kind of time format
+    private int    date; //TODO date should have some date format
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++CONTRUCTORS+++++++++++++
+    public PomodoroUnit(){
+    }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++METHODS++++++++++++++++
+    
+}
