f3e7f657c3b607047d51b2a91588cad0a85f1986
Marek Koszałka <marek.koszalka94@gmail.com>
2016-10-18 12:44:47 +0200

    Learning how to commit properly

---
+++ src/pomodorotracker/MainWinController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pomodorotracker;
+
+/**
+ *
+ * @author Marek
+ */
+public class MainWinController { //TODO przerobic kod tak aby pasowal do konwencji MVC
+    
+}
